ea388d833b24a0f8a918596c054b79907fc41661
nunobreis <nuno@catalpa.io>
2018-04-03 12:35:05 +0100

    adding delete methods to playground

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -9,5 +9,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server.');
 
+  // delete Many
+  // db.collection('Todos').deleteMany({ text: 'tou farto disto' }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').deleteMany({ name: 'Nuno' }).then((result) => {
+    console.log(result);
+  });
+
+  // delete One
+  // db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // find One And Delete
+  // db.collection('Todos').findOneAndDelete({ completed: true }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5a673eb889937781e4e8f359')
+  }).then((result) => {
+    console.log(result);
+  });
+
   // db.close();
 });
