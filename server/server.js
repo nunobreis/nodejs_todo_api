c2c25c84dd00442cae84e43a6a7d3c2e76884d49
nunobreis <nuno@catalpa.io>
2018-04-09 16:11:52 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
     res.send({ todos });
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+// GET todos customised routes - /todos/12345
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo }).catch((e) => {
+      res.status(404).send(e);
+    });
   });
 });
 
