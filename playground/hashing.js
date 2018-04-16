54b46ade24a57ed25c53fa981218243c16090283
nunobreis <nuno@catalpa.io>
2018-04-16 12:20:42 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 10
-};
+const password = '123abc!';
 
-const token = jwt.sign(data, 'YMt.?[c^YGE6M`~F49#JUD5%4');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-const decoded = jwt.verify(token, 'YMt.?[c^YGE6M`~F49#JUD5%4');
-console.log('decoded', decoded);
+const hashedPassword = '$2a$10$iTTyedBgTN/Uk2vHlnVUSOOQPbyUAjiAC6negJ8ZRPD5LWjMWs/zy';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// const data = {
+//   id: 10
+// };
+//
+// const token = jwt.sign(data, 'YMt.?[c^YGE6M`~F49#JUD5%4');
+// console.log(token);
+//
+// const decoded = jwt.verify(token, 'YMt.?[c^YGE6M`~F49#JUD5%4');
+// console.log('decoded', decoded);
 
 //
 // const message = 'I am user number #3';
