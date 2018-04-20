c82d42c79178fc4645a84de96ef9ed9612ceb790
nunobreis <nuno@catalpa.io>
2018-04-20 14:04:34 +0200

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -276,7 +276,26 @@ describe('POST /users', () => {
         });
     });
 
-    // it('should reject invalid login', (done) => {
-    //
-    // });
+    it('should reject invalid login', (done) => {
+      request(app)
+        .post('/users/login')
+        .send({
+          email: users[1].email,
+          password: users[1].password + 1
+        })
+        .expect(400)
+        .expect((res) => {
+          expect(res.headers['x-auth']).toNotExist();
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          User.findById(users[1]._id).then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e) => done(e));
+        });
+    });
   });
